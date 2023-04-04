ec35ffbe9c5ea0fb09758410bea0435a70dc8ca6
melkherv <camelotvm48@gmail.com>
2023-04-04 12:21:28 +0300

    remade register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -23,8 +23,8 @@ class RegisterController extends Controller
         ]);
 
         $user = User::create([
-            'first_name' => $request->name,
-            'last_name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
