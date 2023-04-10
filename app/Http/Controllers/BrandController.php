745b661215cb89ad2b0b7489d4dc2fc04d2c1dee
melkherv <camelotvm48@gmail.com>
2023-04-10 12:45:05 +0300

    Brand controller api methods

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Requests\BrandRequest;
+use App\Http\Requests\BrandUpdateRequest;
 use Symfony\Component\HttpFoundation\Response;
 
 class BrandController extends Controller
@@ -21,32 +24,50 @@ class BrandController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(BrandRequest $request)
     {
-        //
+        $brand = Brand::create([
+            'title' => $request->input('title'),
+            'slug' => Str::slug($request->input('title')),
+        ]);
+
+        return response($brand, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Brand $brand)
+    public function show(string $id)
     {
-        //
+        $brand = Brand::find($id);
+
+        return response($brand, Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Brand $brand)
+    public function update(BrandUpdateRequest $request, string $id)
     {
-        //
+        $brand = Brand::find($id);
+
+        $brand->update([
+            'title' => $request->input('title'),
+            'slug' => Str::slug($request->input('title')),
+        ]);
+
+        return response($brand, Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Brand $brand)
+    public function destroy(string $id)
     {
-        //
+        Brand::destroy($id);
+
+        return response()->json([
+            'message' => 'Brand deleted successfully',
+        ]);
     }
 }
